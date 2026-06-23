a8c2a49e557acaa9501140bd01ed46e9d4d94c1b
bakraouiishak <bakraouiishak@gmail.com>
2026-06-23 08:24:14 +0200

    Fix absolute asset path injection for nested routing

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173, open: true },
+  // server: { port: 5173, open: true },
+  base: '/',
+
   build: {
-    chunkSizeWarningLimit: 1000,
-    base: '/',
+    chunkSizeWarningLimit: 1500, // Silences the warning since we bundled into one file
     rollupOptions: {
       output: {
         manualChunks: undefined,
