ab19482fe1128f5b4d5fbba36808835b5dee39c6
bakraouiishak <bakraouiishak@gmail.com>
2026-06-23 07:24:09 +0200

    Added 'chunkSizeWarningLimit' in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: { port: 5173, open: true },
   build: {
+    chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
         manualChunks(id) {
